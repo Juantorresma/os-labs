757d9833b0584d5a1e8531e8022dbc9b212c83f1
oscreader <oscreader@OSC>
2018-08-31 07:58:58 -0600

    "Se añadieron archivos"

--- lab1.4/simple.c
+++ lab1.4/simple.c
@@ -1,26 +1,47 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/jiffies.h>
+#include <linux/utsname.h>
 
-/* This function is called when the module is loaded. */
+void uptime(void);
+void getSystemInfo(void);
+
 int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
-
+       printk(KERN_INFO "Loading Juan Module \n");
+       uptime();
+       getSystemInfo();
        return 0;
 }
 
-/* This function is called when the module is removed. */
 void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+	printk(KERN_INFO "Removing Juan Module\n");
 }
 
-/* Macros for registering module entry and exit points. */
+
+void uptime() {
+  printk("Uptime: %i\n", jiffies_to_msecs(get_jiffies_64())/1000);
+}
+
+
+void getSystemInfo(){  
+  struct new_utsname *buf;
+  buf = utsname();
+
+  printk("Nodename: %s\n",buf->nodename);
+  printk("Machine: %s\n",buf->machine);
+  printk("Sysname: %s\n",buf->sysname);
+  printk("Release: %s\n",buf->release);
+  printk("Version: %s\n",buf->version);
+#ifdef __linux__
+  printk("Domain Name: %s\n", buf->domainname); 
+#endif
+}
+
+
 module_init( simple_init );
 module_exit( simple_exit );
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
 MODULE_AUTHOR("SGG");
-
-
